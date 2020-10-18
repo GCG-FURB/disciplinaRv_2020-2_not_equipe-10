b8e44fb536481b2765dbcece8e8d1a6702b1adbe
Matheus Klauberg <mfklauberg@gmail.com>
2020-10-18 20:39:06 -0300

    xcode project structure

---
+++ art/art/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  Posts.swift
+//  art
+//
+//  Created by Matheus Klauberg on 18/10/20.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Posts_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}
---
+++ art/art/TimelineView.swift
@@ -0,0 +1,20 @@
+//
+//  TimelineView.swift
+//  art
+//
+//  Created by Matheus Klauberg on 18/10/20.
+//
+
+import SwiftUI
+
+struct TimelineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimelineView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimelineView()
+    }
+}
